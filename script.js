488aee924f7b8f743df8092ccfcbc6d9ad88e535
MadhanprasanthM <144345414+MadhanprasanthM@users.noreply.github.com>
2024-07-14 22:06:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var category=document.querySelector(".shop")
 var search=document.querySelector(".search-item")
 var slideimage=document.querySelector(".image")
-var showimage=document.querySelector(".image1")
+var showcontent=document.querySelector(".content")
 
 category.addEventListener('click',()=>{
     search.style.display="block"
     slideimage.style.display="none"
-    showimage.style.display="none"
+    showcontent.style.display="none"
 })
